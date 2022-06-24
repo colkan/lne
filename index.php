ebd615ba0885d9bc8653e92737f59006a56dae97
colkan <remeratarafaeljay77@gmail.com>
2022-06-24 04:21:59 -0700

    design tweet

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include 'backend/shared/header.php';?>
+
+
+    <section class="main-page">
+        <div class="left">
+            <div class="left-content">
+
+        
+                <div>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <h3 class="left-content-heading">Find your Interest</h3>
+                </div>
+
+                <div>
+                <i class="fa-solid fa-users"></i>
+                <h3 class="left-content-heading">Explore what people are talking about</h3>
+                </div>
+
+
+                <div>
+                <i class="fa-solid fa-comment"></i>
+                <h3 class="left-content-heading">Join the people</h3>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="right">
+            <div class="middle-content">
+            <i class="fa-solid fa-puzzle-piece"></i>
+            <h1>See what's happening in he world right now.
+
+            </h1>
+            <h4>Join Tweet now</h4>
+            <a href="signUp.php" class="sign-up">Sign Up</a>
+            <a href="login.php" class="log-in">Log In</a>
+            </div>
+        </div>
+        <footer class="main-page-footer">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Help Center</a></li>
+                <li><a href="">Terms of Service</a></li>
+                <li><a href="">Privacy</a></li>
+                <li><a href="">Cookie Policy</a></li>
+                <li><a href="">Accessibility</a></li>
+                <li><a href="">Ads Info</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Status</a></li>
+                <li><a href="">Career</a></li>
+                <li><a href="">Brand Resources</a></li>
+                <li><a href="">Advertising</a></li>
+                <li><a href="">Marketing</a></li>
+                <li><a href="">Twitter for Buiness</a></li>
+                <li><a href="">Developers</a></li>
+                <li><a href="">Directory</a></li>
+                <li><a href="">Settings</a></li>
+                <li><a href="">&copy; <?php echo date('d/m/Y'); ?> Twitter, Inc.</a></li>
+                
+            </ul>
+        </footer>
+
+    </section>
+</body>
+</html>
